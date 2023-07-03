a9eeee765e272780a6bbba16139a0cecc4cb79b5
Cristopher F <30444850+crisatori@users.noreply.github.com>
2023-07-03 06:58:47 -0400

    ...

--- src/com/crisatori/Conversor.java
+++ src/com/crisatori/Conversor.java
@@ -4,9 +4,10 @@ public class Conversor {
     private final double tasaDOPUSD = 55.12; //cantidad de pesos que me dan por 1 dolar.
     private final double tasaMXNUSD = 17.12; //...mexicanos
 
-    public void convertirMonedas(int cantidad) {
-        System.out.println((cantidad * tasaDeReferncia));
-        //System.out.println((cantidad * moneda1.getValor()) / tasaDeReferncia);
+    public void convertirMonedas(double cantidad) {
+        double DOP_A_USD = cantidad / this.tasaDOPUSD;
+        double DOP_A_MXN = DOP_A_USD * this.tasaMXNUSD;
+        System.out.println(DOP_A_MXN + "MXN");
     }
 
     /*public void convertirMonedas(int cantidad, Moneda moneda1, Moneda moneda2) {
